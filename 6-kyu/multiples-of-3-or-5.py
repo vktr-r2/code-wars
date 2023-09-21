a95e11072d8e93dca226f651d0d349379de5871a
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-20 22:13:34 -0400

    Implement solution

--- 6-kyu/multiples-of-3-or-5.py
+++ 6-kyu/multiples-of-3-or-5.py
@@ -3,3 +3,21 @@
 # Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
 
 # Note: If the number is a multiple of both 3 and 5, only count it once.
+
+def solution(num): 
+  if num <= 0:
+    return 0
+  
+  sum = 0
+
+  for i in range(0, num):
+   if i % 3 == 0 or i % 5 == 0:
+     sum += i
+     print(i)
+     print(sum)
+     print("--------------")
+
+  return sum
+
+
+print(solution(10))
