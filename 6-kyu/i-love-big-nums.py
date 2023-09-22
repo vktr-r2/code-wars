d923cef9e73e26f365415f352e78b7863fb41a97
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-22 17:06:57 -0400

    Implement i-love-big-nums.py solution

--- 6-kyu/i-love-big-nums.py
+++ 6-kyu/i-love-big-nums.py
@@ -9,3 +9,25 @@ For example:
 The results will be large so make sure to return a string.
 
 """
+
+
+from functools import cmp_to_key
+
+def custom_compare(a, b):
+    ab = a + b
+    ba = b + a
+    return (ab > ba) - (ab < ba)
+
+def biggest(nums):
+    # If all numbers are zero, return '0'
+    if all(num == 0 for num in nums):
+        return '0'
+    
+    # Convert numbers to strings for lexicographical comparison
+    str_nums = list(map(str, nums))
+    
+    # Custom sort the array
+    str_nums.sort(key=cmp_to_key(custom_compare), reverse=True)
+    
+    # Concat sorted strings
+    return ''.join(str_nums)
