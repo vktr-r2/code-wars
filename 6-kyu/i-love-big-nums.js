5eadaaf92d9247de475a29e68903100ce329d2e3
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-22 16:55:38 -0400

    Implement i-love-big-nums.js solution

--- 6-kyu/i-love-big-nums.js
+++ 6-kyu/i-love-big-nums.js
@@ -1,6 +1,7 @@
 /*
 
-Write a function that given an array of numbers >= 0, will arrange them such that they form the biggest number.
+Write a function that given an array of numbers >= 0, 
+will arrange them such that they form the biggest number.
 
 For example:
 
@@ -10,3 +11,27 @@ The results will be large so make sure to return a string.
 
 */
 
+
+// MY SOLUTION
+function biggest(nums) {
+
+  // If all numbers are zero, return '0'
+  if (nums.every(num => num === 0)) {
+    return '0';
+  }
+
+  // Convert numbers to strings for easy comparison
+  const strNums = nums.map(String);
+  
+  // Custom sort the array
+  strNums.sort((a, b) => {
+    const ab = a + b;
+    const ba = b + a;
+
+    // localeCompare method is used to compare two strings lexicographically
+    return ba.localeCompare(ab);
+  });
+
+  // Join the sorted strings to form the biggest number and return it
+  return strNums.join('');
+};
