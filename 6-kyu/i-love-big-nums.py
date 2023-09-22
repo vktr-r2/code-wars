c301c72329d093b8f9df91938b032d2f8047d8b1
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-22 14:24:00 -0400

    Setup i-love-big-nums.py file

---
+++ 6-kyu/i-love-big-nums.py
@@ -0,0 +1,11 @@
+"""
+
+Write a function that given an array of numbers >= 0, will arrange them such that they form the biggest number.
+
+For example:
+
+[1, 2, 3] --> "321" (3-2-1)
+[3, 30, 34, 5, 9] --> "9534330" (9-5-34-3-30)
+The results will be large so make sure to return a string.
+
+"""
