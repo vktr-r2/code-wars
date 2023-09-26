b4f1fcf02d091ae572b290b1bf6b3bea21d285c8
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-26 16:55:41 -0400

    Implement find-the-odd-int.py solution

--- 6-kyu/find-the-odd-int.py
+++ 6-kyu/find-the-odd-int.py
@@ -12,6 +12,27 @@ Examples
 [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
 """
-
+# MY SOLUTION
 def find_it(seq):
-    return None
+    
+    # Define dictionary to count number of occurances
+    counter = {}
+    
+
+    """'counter.get(ele, 0)' : This method returns the value for the given key 
+    ele if it exists in the dictionary. If the key does not exist, 
+    it returns the default value 0.
+
+    '+ 1' : This part increments the count by 1. So, if ele already exists in the dictionary, its count will be incremented. 
+    If it does not exist, the get() method returns 0, and then 0 + 1 will be 1.
+
+    'counter[ele] =' : Finally, the updated count is stored back in the dictionary 
+    at the key ele."""
+
+    for ele in seq:
+        counter[ele] = counter.get(ele, 0) + 1
+
+    # Check if value is odd, if so return key
+    for key in counter:
+        if counter[key] % 2 != 0:
+            return key
