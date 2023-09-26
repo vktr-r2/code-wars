ef3142fe516a37a3388feba57fcc8602b94a5eef
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-26 19:25:52 -0400

    Setup character-counts.js kata file

---
+++ 6-kyu/character-counts.js
@@ -0,0 +1,25 @@
+/*
+
+Character counts
+The object is to count the number of occurances of a specified character 
+(or set of characters) in a string.
+
+Instructions
+Complete the placeholder function characterCount.
+
+it should return the number of times a single character appears in the string, 
+or if multiple characters are specified (either by providing an array or string 
+of length 2 or more), it should return an array of character counts
+see the unit tests provided for a more comprehensive definition
+NOTE: The tests assume that if no arguments are provided to the function 
+(i.e. ''.characterCount()), then the result is undefined.
+
+*/
+
+/**
+ * Returns the number of times the given character appears in the string,
+ * or if more than one character is supplied an array of character counts.
+ */
+String.prototype.characterCount = function (charsToCount) {
+  // your solution
+};
