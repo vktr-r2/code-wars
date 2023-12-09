029593ac5f20e700565c1329e69dc8f03ff18768
vktr-r2 <vktr-r2@users.noreply.github.com>
2023-12-08 22:45:57 -0500

    Resolve extract-domain-name.py kata

--- 5-kyu/extract-domain-name.py
+++ 5-kyu/extract-domain-name.py
@@ -8,8 +8,22 @@ domain name and returns it as a string. For example:
 * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
 * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 * url = "https://www.cnet.com"                -> domain name = cnet"
+* url = "www.xakep.ru"                        -> domain name = xakep"
 
 """
+import re
 
 def domain_name(url):
-    pass
+    # Updated regex to extract just the domain name, excluding top-level domain
+    domain_pattern = re.compile(r'(https?://)?(www\.)?([^/:.]+)')
+    match = domain_pattern.search(url)
+    if match:
+        return match.group(3)
+    else:
+        return None  
+
+domain_name("http://github.com/carbonfive/raygun")
+domain_name("http://www.zombie-bites.com")
+domain_name("https://www.cnet.com")
+domain_name("www.xakep.ru")
+
