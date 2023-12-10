cee9526c11a604eb1f0c4505403544cb00997cc7
vktr-r2 <vktr-r2@users.noreply.github.com>
2023-12-10 14:11:01 -0500

    Resolve leap_year.py kata

--- 7-kyu/leap_year.py
+++ 7-kyu/leap_year.py
@@ -8,5 +8,7 @@ but years divisible by 400 are leap years.
 Tested years are in range 1600 ≤ year ≤ 4000.
 
 """
+def is_leap_year(year):
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
 
 
