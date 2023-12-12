cb1e019d9b28256d67ea9ffd05093cf02e0db0bd
vktr-r2 <vristic@freshbooks.com>
2023-12-12 17:56:13 -0500

    Resolve oop_object_oriented_piracy.py kata

--- 8-kyu/resolved-katas/oop_object_oriented_piracy.py
+++ 8-kyu/resolved-katas/oop_object_oriented_piracy.py
@@ -21,12 +21,17 @@ You have access to the ship "draft" and "crew". "Draft" is the total ship weight
 
 Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, then the ship is worth looting. Any ship weighing that much must have a lot of booty!
 
-Add the method
-
-is_worth_it
-to decide if the ship is worthy to loot. For example:
+Add the method is_worth_it to decide if the ship is worthy to loot. For example:
 
 Titanic.is_worth_it()
 False
 Good luck and may you find GOOOLD!
 """
+
+
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+    def is_worth_it(self):                                          # define method is_worth_it and pass self from object
+        return self.draft - (self.crew * 1.5) > 20                  # return expression evaluation
