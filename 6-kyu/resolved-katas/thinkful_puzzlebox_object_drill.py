405b9d15a495aece62eabe4cf357cdcf45f19e29
vktr-r2 <122321007+vktr-r2@users.noreply.github.com>
2023-12-18 00:02:26 -0500

    Resolve puzzle_box_object_drill.py kata

---
+++ 6-kyu/resolved-katas/thinkful_puzzlebox_object_drill.py
@@ -0,0 +1,17 @@
+"""
+https://www.codewars.com/kata/587f0871f297a6df780000cd/python
+
+You're given a mystery puzzlebox object. Examine it to make the tests pass and solve this kata.
+
+"""
+
+
+
+def answer(puzzlebox):
+    #print(type(puzzlebox))                         => Object is instantiated from class
+    #print(dir(puzzlebox))                          => directory method shows hint class variables avail
+    #puzzlebox.hint
+    #print(puzzlebox.hint_two)
+    #print(puzzlebox.lock(puzzlebox.key))           => call .lock function with .key class variable as arg
+
+    return 42                                      #=> told to just return 42 int
