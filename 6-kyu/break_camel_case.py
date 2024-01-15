79a85f94cfacd464c4330eb2681d2f9fa76ac49e
vktr-r2 <122321007+vktr-r2@users.noreply.github.com>
2024-01-14 21:07:28 -0500

    break_camel_case.py in progress

---
+++ 6-kyu/break_camel_case.py
@@ -0,0 +1,38 @@
+"""
+DESCRIPTION:
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+"""
+
+# First attempt not working when multiple uppercase characters exist in string
+# def solution(s):
+
+#     for index, char in enumerate(s):
+#         print(index)
+#         if char.isupper():
+#             s = s[:index] + " " + s[index:]
+
+#     return s
+
+# Second attempt not working when multiple uppercase characters exist in string
+
+
+def solution(s):
+
+    uppercase_indicies = []
+
+    for index, char in enumerate(s):
+        if char.isupper():
+            uppercase_indicies.append(index)
+
+    for index in uppercase_indicies:
+        s = s[:index] + " " + s[index:]
+
+    return s
+
+
+print(solution("camelCasingTestString"))
