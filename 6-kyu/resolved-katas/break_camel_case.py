6e0248be3fe1417a574adc0fb2ecc6e9320d381b
vktr-r2 <122321007+vktr-r2@users.noreply.github.com>
2024-01-16 00:04:01 -0500

    Revert to original resolution attempt with single loop

---
+++ 6-kyu/resolved-katas/break_camel_case.py
@@ -0,0 +1,44 @@
+"""
+DESCRIPTION:
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+"""
+
+# First attempt not working when multiple uppercase characters exist in string
+
+
+def solution(s):
+    tally = 0
+
+    for index, char in enumerate(s):
+
+        if char.isupper():
+            s = s[:index + tally] + " " + s[index + tally:]
+            tally += 1
+
+    return s
+
+# Second attempt not working when multiple uppercase characters exist in string
+
+
+# def solution(s):
+
+#     uppercase_indicies = []
+#     tally = 0
+
+#     for index, char in enumerate(s):
+#         if char.isupper():
+#             uppercase_indicies.append(index)
+
+#     for index in uppercase_indicies:
+#         s = s[:index + tally] + " " + s[index + tally:]
+#         tally += 1
+
+#     return s
+
+
+print(solution("camelCasingTestString"))
