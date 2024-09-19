c67daf07ecf79e7b67bcfbbf748d9781f7907581
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-09-19 18:39:24 -0400

    Setup is_number_a_prime kata

---
+++ 6-kyu/is_number_a_prime.rb
@@ -0,0 +1,14 @@
+=begin
+Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Requirements
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+=end
+
+# Test if number is prime
+def is_prime(num)
+  
+end
